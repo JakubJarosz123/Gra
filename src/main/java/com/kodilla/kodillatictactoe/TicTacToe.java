bf0af3f708747db3337d92b6959b880983b0f859
Jakub Jarosz <jakubjarosz2015@gmail.com>
2025-10-29 16:20:33 +0100

    Początkowy panel GUI gry

---
+++ src/main/java/com/kodilla/kodillatictactoe/TicTacToe.java
@@ -0,0 +1,35 @@
+package com.kodilla.kodillatictactoe;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+
+public class TicTacToe extends Application {
+
+    private Image image = new Image("file:src/main/resources/tictactoe.jpg");
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
+        BackgroundImage backgroundImage = new BackgroundImage(image,BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        Background background = new Background(backgroundImage);
+
+        GridPane gridPane = new GridPane();
+        gridPane.setBackground(background);
+
+        Scene scene = new Scene(gridPane, 300, 300, Color.BLACK);
+
+        primaryStage.setTitle("TicTacToe");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+}
+
